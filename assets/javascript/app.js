8a331ca101084ae710f2333561e97259959fe6d8
CoryDaughtry <47927665+CoryDaughtry@users.noreply.github.com>
2019-04-04 13:52:47 -0500

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,148 @@
+
+var panel = $('#quiz-area');
+var countStartNumber = 30;
+
+
+$(document).on('click', '#start-over', function(e) {
+  game.reset();
+});
+
+$(document).on('click', '.answer-button', function(e) {
+  game.clicked(e);
+});
+
+$(document).on('click', '#start', function(e) {
+  $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+  game.loadQuestion();
+});
+
+
+var questions = [{
+  question: "The M4 Carbine shoots what size round?",
+  answers: ["7.62", "50mm", "5.56", "20mm"],
+  correctAnswer: "5.56",
+  image:"assets/images/m4.jpg"
+}, {
+  question: "The M249 Light Machine Gun is commonly refered to as?",
+  answers: ["SAW", "PIC", "AXE", "MOW"],
+  correctAnswer: "SAW",
+  image:"assets/images/M249_Automatic_Rifle.jpg"
+}, {
+  question: "The M240 Bravo Machine Gun has a Maximum firing range of?",
+  answers: ["1254 Meters", "2543 Meters", "3059 Meters", "3725 Meters"],
+  correctAnswer: "3725 Meters",
+  image:"assets/images/M240.jpg"
+}, {
+  question: "The M2 Browning 50cal Machine Gun weights how much WITHOUT its tripod?",
+  answers: ["83 Pounds", "52 Pounds", "64 Pounds", "75 Pounds"],
+  correctAnswer: "83 Pounds",
+  image:"assets/images/50cal.jpg"
+}, {
+  question: 'The MK19 is an automatic ______ launcher?',
+  answers: ["Rocket", "Grenade", "Missile"],
+  correctAnswer: "Grenade",
+  image:"assets/images/mk19.jpg"
+}, {
+  question: 'How much does it cost to shoot 1 round from a Javelin?',
+  answers: ["Around $20thousand", "Around $40thousand", "Around $100thousand"],
+  correctAnswer: "Around $100thousand",
+  image:"assets/images/jav.jpg"
+}];
+
+
+
+
+var game = {
+  questions:questions,
+  currentQuestion:0,
+  counter:countStartNumber,
+  correct:0,
+  incorrect:0,
+  countdown: function(){
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0){
+      console.log('TIME UP');
+      game.timeUp();
+    }
+  },
+  loadQuestion: function(){
+    timer = setInterval(game.countdown, 1000);
+    panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+      panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+    }
+  },
+  nextQuestion: function(){
+    game.counter = countStartNumber;
+    $('#counter-number').html(game.counter);
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
+  timeUp: function (){
+    clearInterval(timer);
+    $('#counter-number').html(game.counter);
+
+    panel.html('<h2>Out of Time!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+    panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  results: function() {
+    clearInterval(timer);
+
+    panel.html('<h2>Nice Job!</h2>');
+    $('#counter-number').html(game.counter);
+    panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+    panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+    panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+    panel.append('<br><button id="start-over">Start Over?</button>');
+  },
+  clicked: function(e) {
+    clearInterval(timer);
+
+    if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+      this.answeredCorrectly();
+    } else {
+      this.answeredIncorrectly();
+    }
+  },
+  answeredIncorrectly: function() {
+    game.incorrect++;
+    clearInterval(timer);
+    panel.html('<h2>No Sorry!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+    panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  answeredCorrectly: function(){
+    clearInterval(timer);
+    game.correct++;
+    panel.html('<h2>Yes!</h2>');
+    panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  reset: function(){
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
